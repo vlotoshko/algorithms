939103e338b3e48fc3f48d2ec2b97b1034e6db71
Volodymyr Lotoshko <vlotoshko@gmail.com>
2019-01-05 21:27:51 +0200

    [*] Added BinarySearchTree unit tests.

--- Tests/TestBinarySearchTree.hpp
+++ Tests/TestBinarySearchTree.hpp
@@ -15,6 +15,10 @@
 
 #include <fstream>
 #include <sstream>
+#include <cppunit/TestFixture.h>
+#include <cppunit/TestCaller.h>
+#include <cppunit/TestSuite.h>
+#include <cppunit/extensions/HelperMacros.h>
 //--------------------------------------------------------------------------------------------------
 
 //--------------------------------------------------------------------------------------------------
@@ -77,6 +81,93 @@ private:
 
 
 } // namespace binary search tree
+
+namespace tests
+{
+
+class TestBST2 : public CppUnit::TestFixture
+{
+public:
+    static CppUnit::Test * suite()
+    {
+        CppUnit::TestSuite *suiteOfTests = new CppUnit::TestSuite("TestBinarySearchTree");
+        suiteOfTests->addTest(new CppUnit::TestCaller<TestBST2>(
+                                  "get_ShouldReturnValue_WhenGivenKey",
+                                  &TestBST2::get_ShouldReturnValue_WhenGivenKey));
+        suiteOfTests->addTest(new CppUnit::TestCaller<TestBST2>(
+                                  "deleteNode_ShouldDeleteNode_WhenGivenKey",
+                                  &TestBST2::deleteNode_ShouldDeleteNode_WhenGivenKey));
+        suiteOfTests->addTest(new CppUnit::TestCaller<TestBST2>(
+                                  "min_ShouldReturnKeyWithMinimalValue",
+                                  &TestBST2::min_ShouldReturnKeyWithMinimalValue));
+        suiteOfTests->addTest(new CppUnit::TestCaller<TestBST2>(
+                                  "max_ShouldReturnKeyWithMaximalValue",
+                                  &TestBST2::max_ShouldReturnKeyWithMaximalValue));
+        suiteOfTests->addTest(new CppUnit::TestCaller<TestBST2>(
+                                  "size_ShouldReturnCountOfNodes",
+                                  &TestBST2::size_ShouldReturnCountOfNodes));
+        suiteOfTests->addTest(new CppUnit::TestCaller<TestBST2>(
+                                  "valueSum_ShouldReturnSumOfAllvalues",
+                                  &TestBST2::valueSum_ShouldReturnSumOfAllvalues));
+        return suiteOfTests;
+    }
+
+    void setUp()
+    {
+        bst.put("word", 0);
+        bst.put("select", 0);
+        bst.put("connect", 0);
+        bst.put("car", 0);
+        bst.put("car", 0);
+        bst.put("cat", 0);
+        bst.put("android", 0);
+        bst.put("word", 0);
+    }
+
+protected:
+    void get_ShouldReturnValue_WhenGivenKey()
+    {
+        CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(2), bst.get("word"));
+        CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), bst.get("select"));
+        CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(0), bst.get("zero"));
+    }
+
+    void deleteNode_ShouldDeleteNode_WhenGivenKey()
+    {
+        CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(2), bst.get("car"));
+        bst.deleteNode("car");
+        CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(0), bst.get("car"));
+    }
+
+    void min_ShouldReturnKeyWithMinimalValue()
+    {
+        CPPUNIT_ASSERT_EQUAL(static_cast<std::string>("android"), bst.min());
+    }
+
+    void max_ShouldReturnKeyWithMaximalValue()
+    {
+        CPPUNIT_ASSERT_EQUAL(static_cast<std::string>("word"), bst.max());
+    }
+
+    void size_ShouldReturnCountOfNodes()
+    {
+        CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(6), bst.size());
+        bst.deleteNode("car");
+        bst.deleteNode("select");
+        CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(4), bst.size());
+    }
+
+    void valueSum_ShouldReturnSumOfAllvalues()
+    {
+        CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(8), bst.valueSum());
+    }
+
+private:
+    bst::BinarySearchTree<std::string, size_t> bst;
+};
+
+
+} // namespace tests
 
 //--------------------------------------------------------------------------------------------------
 #endif // TESTBINARYSEARCHTREE_HPP
